d44c0ff6285d23ac03495e33ab6844999dabc8a9
Derek_yip <derek.yipchinsan@gmail.com>
2024-01-15 19:19:39 +0800

    remove trycatch in getProduct

--- actions/get-product.tsx
+++ actions/get-product.tsx
@@ -3,14 +3,11 @@ import axios from "axios";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
 
-const getProduct = async (id: string): Promise<Product | void> => {
+const getProduct = async (id: string): Promise<Product> => {
   // const res = await fetch(`${URL}/${id}`);
-  try {
-    const response = await axios.get(`${URL}/${id}`);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+  const response = await axios.get(`${URL}/${id}`);
+  return response.data;
+
   // return res.json();
 };
 
