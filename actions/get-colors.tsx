811c0a6b8439d47a1d74b7d6ff56d49fe5f8409c
Derek_yip <derek.yipchinsan@gmail.com>
2024-01-15 19:10:43 +0800

    remove return void value in color and size

--- actions/get-colors.tsx
+++ actions/get-colors.tsx
@@ -3,14 +3,10 @@ import axios from "axios";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/colors/`;
 
-const getColors = async (): Promise<Color[] | void> => {
+const getColors = async (): Promise<Color[]> => {
   // const res = await fetch(URL);
-  try {
     const response = await axios.get(URL);
     return response.data;
-  } catch (error) {
-    console.log(error);
-  }
   // return res.json();
 };
 
--- actions/get-sizes.tsx
+++ actions/get-sizes.tsx
@@ -3,14 +3,11 @@ import axios from "axios";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/sizes/`;
 
-const getSizes = async (): Promise<Size[] | void> => {
+const getSizes = async (): Promise<Size[]> => {
   // const res = await fetch(URL);
-  try {
     const response = await axios.get(URL);
     return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+
   // return res.json();
 };
 
