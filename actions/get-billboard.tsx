9907f705a6c7bc495e91252d393ce2bf143653c0
Derek_yip <derek.yipchinsan@gmail.com>
2024-01-15 19:14:01 +0800

    delete catch in getBillboard

--- actions/get-billboard.tsx
+++ actions/get-billboard.tsx
@@ -3,15 +3,12 @@ import axios from "axios";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/billboards`;
 
-const getBillboard = async (id: string): Promise<Billboard | void> => {
+const getBillboard = async (id: string): Promise<Billboard> => {
   // const res = await fetch(`${URL}/${id}`);
   // return res.json();
-  try {
-    const response = await axios.get(`${URL}/${id}`);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+
+  const response = await axios.get(`${URL}/${id}`);
+  return response.data;
 };
 
 export default getBillboard;
