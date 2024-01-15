2cd30dcd5f8dd02a83177d719ecc86c900699b76
Derek_yip <derek.yipchinsan@gmail.com>
2024-01-15 19:12:11 +0800

    handle error in product

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -43,9 +43,9 @@ async function CategoryPage({ params, searchParams }: CategoryPageProps) {
               <Filter valueKey="colorId" name="Colors" data={colors} />
             </div>
             <div className="mt-6 lg:col-span-4 lg:mt-0">
-              {products.length === 0 && <NoResults />}
+              {products?.length === 0 && <NoResults />}
               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                {products.map((item) => (
+                {products?.map((item) => (
                   <ProductCard key={item.id} data={item} />
                 ))}
               </div>
